dfb3f5ad6685ec6e8b9a7aa36f439c7198b16fbd
Rahulommm <51383903+Rahulommm@users.noreply.github.com>
2025-07-19 14:06:52 +0530

    Add files via upload

---
+++ modules/code_compliance.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def render(df):
+    st.warning("This module is under development.")
---
+++ modules/montecarlo.py
@@ -0,0 +1,3 @@
+import streamlit as st
+def render(df):
+    st.warning("This module is under development.")
